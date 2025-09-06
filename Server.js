e06bf70100a0a8d9631f93bd140d1f69bfe1a90a
sirothiya <shalini21sirothiya@gmail.com>
2025-09-06 19:09:13 +0530

    cors correction

--- Server.js
+++ Server.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const app = express();
-const cors = require("cors");
 require("dotenv").config();
 const bodyParser = require("body-parser");
+const cors = require("cors");
+// Allow specific frontend (during development)
+app.use(
+  cors({
+    origin: "http://localhost:5173", // your frontend URL
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
 app.use(bodyParser.json());
 const PORT = process.env.PORT || 3000;
 
@@ -17,14 +26,7 @@ app.get("/", (req, res) => {
   res.status(200).send("Welcome to the Voting App");
 });
 
-// Allow specific frontend (during development)
-app.use(
-  cors({
-    origin: "http://localhost:5173", // your frontend URL
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
