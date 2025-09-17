d3bb8d000c522152d00bda7e316df6fb5ab9cc1a
sirothiya <shalini21sirothiya@gmail.com>
2025-09-17 09:26:04 +0530

    error in gemini

--- Routes/Gemini.js
+++ Routes/Gemini.js
@@ -28,35 +28,6 @@ router.post("/generate-image", async (req, res) => {
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
-  //   const googleRes = await fetch(
-  //     `https://generativelanguage.googleapis.com/v1beta/models/imagen-4.0-generate-001:generateImages?key=${process.env.GEMINI_API_KEY}`,
-  //     {
-  //       method: "POST",
-  //       headers: { "Content-Type": "application/json" },
-  //       body: JSON.stringify({
-  //         prompt,
-  //         config: { numberOfImages: 1 },
-  //       }),
-  //     }
-  //   );
-
-  //   const data = await googleRes.json();
-  //   if (!googleRes.ok) {
-  //     return res.status(400).json(data);
-  //   }
-
-  //   // get base64 from Google
-  //   const imgBytes = data.generatedImages[0].image.imageBytes;
-
-  //   // convert to Buffer
-  //   const buffer = Buffer.from(imgBytes, "base64");
-
-  //   // send file directly
-  //   res.setHeader("Content-Type", "image/png");
-  //   res.send(buffer);
-  // } catch (err) {
-  //   res.status(500).json({ error: err.message });
-  // }
 });
 
-export default router;
+module.exports = router;
