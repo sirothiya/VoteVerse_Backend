f1180c71a277c0965b6e68bbea888394c8d9e383
sirothiya <shalini21sirothiya@gmail.com>
2025-10-08 16:11:00 +0530

    changes in candidate files upload

--- Routes/CandidateRoute.js
+++ Routes/CandidateRoute.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const multer = require("multer");
-const path = require("path");
 
 const Candidate = require("../Models/Candidate");
 const { generateToken, jwtMiddleware } = require("../jwt");
