9bbf0530a03f8c127587b569f8e73e5012680735
sirothiya <shalini21sirothiya@gmail.com>
2025-12-18 09:11:11 +0530

    Changes made on server.js and candidateRoute.js to fix the upload and serving files issue

--- Server.js
+++ Server.js
@@ -30,8 +30,6 @@ app.use("/gemini", geminiRoute);
 app.use("/admin",AdminRoute)
 app.use("/election",ElectionRoute)
 app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
-// app.use('/uploads', express.static(path.join(process.cwd(), "uploads")))
-// app.use('/uploads', express.static('uploads'));
 
 
 app.get("/", (req, res) => {
