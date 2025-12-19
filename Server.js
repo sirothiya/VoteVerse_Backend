624f7dc962725d3f2c9f7c34d34e232abd96156a
sirothiya <shalini21sirothiya@gmail.com>
2025-12-19 09:56:55 +0530

    Changes made for the uploaded file view in frontend

--- Server.js
+++ Server.js
@@ -29,7 +29,7 @@ app.use("/user", userRoute);
 app.use("/gemini", geminiRoute);
 app.use("/admin",AdminRoute)
 app.use("/election",ElectionRoute)
-app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
+app.use("/uploads", express.static(path.resolve(__dirname, "uploads")));
 
 
 app.get("/", (req, res) => {
