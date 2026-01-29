0f04b2cadfbf48f0a199c4ccb81dc868825ee31f
sirothiya <shalini21sirothiya@gmail.com>
2026-01-29 18:47:15 +0530

    Changes in extract manifesto route to get the AI support

--- Routes/CandidateRoute.js
+++ Routes/CandidateRoute.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const fs = require("fs");
 
 const Candidate = require("../Models/Candidate");
 const Election = require("../Models/Election");
