4be61e176db88d8f8da3a555880a68f87c9aae72
sirothiya <shalini21sirothiya@gmail.com>
2026-01-29 22:39:37 +0530

    Fix: add pdf-parse dependency for manifesto extraction

--- utils/extractPdfText.js
+++ utils/extractPdfText.js
@@ -1,5 +1,14 @@
+// async function extractPdfText(buffer) {
+//   const pdfParse = (await import("pdf-parse")).default;
+//   const data = await pdfParse(buffer);
+//   return data.text;
+// }
+
+// module.exports = extractPdfText;
+
+const pdfParse = require("pdf-parse");
+
 async function extractPdfText(buffer) {
-  const pdfParse = (await import("pdf-parse")).default;
   const data = await pdfParse(buffer);
   return data.text;
 }
