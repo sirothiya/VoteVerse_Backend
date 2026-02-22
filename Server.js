528c0dadf9c2cb05d3169265217994dd904d010e
sirothiya <shalini21sirothiya@gmail.com>
2026-02-22 22:13:42 +0530

    Bug in candidate profile page

--- Server.js
+++ Server.js
@@ -23,7 +23,13 @@ app.use(
     },
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: [
+  "Content-Type",
+  "Authorization",
+  "Cache-Control",
+  "Pragma",
+  "Expires"
+],
   })
 );
 
